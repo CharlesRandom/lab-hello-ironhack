0edeb2622684c7235d64cc5fb1c0180b2de619d3
CharlesRandom <carlos.rivera.cabrera@gmail.com>
2021-03-01 21:01:39 -0600

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -9,8 +9,8 @@ class App extends React.Component {
       <div className="main-container">
         <div className="top-container">
           <div className="navbar">
-            <img src="../images/ironhack-logo.svg" alt="Ironhack logo" />
-            <img src="../images/menu-top.svg" alt="menu pic" />
+            <img src='../public/images/ironhack-logo.svg' alt="Ironhack logo" />
+            <img src="../public/images/menu-top.svg" alt="menu pic" />
           </div>
           <div className = "perro">
             <h1>Say hello te ReactJS</h1>
@@ -23,28 +23,28 @@ class App extends React.Component {
         </div>
         <div className="bottom-container">
           <div className="card-container">
-            <img src="../images/icon1.png" alt="Declarative"/>
+            <img src="../public/images/icon1.png" alt="Declarative"/>
             <h3>Declarative</h3>
             <p>
               React makes it painless to create interactive UIs.
             </p>
           </div>
           <div className="card-container">
-            <img src="../images/icon2.png" alt="Components"/>
+            <img src="../public/images/icon2.png" alt="Components"/>
             <h3>Components</h3>
             <p>
               Build encapsulated components that manage their state.
             </p>
           </div>
           <div className="card-container">
-            <img src="../images/icon3.png" alt="Single-Way"/>
+            <img src="../public/images/icon3.png" alt="Single-Way"/>
             <h3>Single-Way</h3>
             <p>
               A set of immutable values are passed to the components.
             </p>
           </div>
           <div className="card-container">
-            <img src="../images/icon4.png" alt="JSX"/>
+            <img src="../public/images/icon4.png" alt="JSX"/>
             <h3>JSX</h3>
             <p>
               Statically-typed designed to run on modern browsers.
